df1e97dd4db146bb9d26b75cda4a0cc742d38bae
LusquesToodoo <lucas.carlos@toodoo.com.br>
2023-06-01 12:04:47 -0300

    style:photo link

--- header-blastoff.php
+++ header-blastoff.php
@@ -1,4 +1,4 @@
-got <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
@@ -12,7 +12,6 @@ got <!DOCTYPE html>
   <script defer src="<?php echo get_stylesheet_directory_uri(); ?>/new-blastoff/public/js/blastoff.js"></script>
   <script defer src="<?php echo get_stylesheet_directory_uri(); ?>/new-blastoff/public/js/responsive.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <!-- <?php wp_head(); ?> -->
 </head>
 
 <body>
