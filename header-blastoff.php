507cd3a3b18c1d147aebb6b710b879502efb11ff
LusquesToodoo <lucas.carlos@toodoo.com.br>
2023-06-06 12:01:44 -0300

    SEO Yoast php tag

--- header-blastoff.php
+++ header-blastoff.php
@@ -12,7 +12,8 @@
   <script defer src="<?php echo get_stylesheet_directory_uri(); ?>/new-blastoff/public/js/blastoff.js?ref=20"></script>
   <script defer src="<?php echo get_stylesheet_directory_uri(); ?>/new-blastoff/public/js/responsive.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <?php wp_head()?>
+  <?php do_action( 'wpseo_head' );?>
+
 </head>
 
 <body>
